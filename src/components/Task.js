953821ff775f91e71548c74fd0005904c91f7b07
Andres Florez <anflo.97@gmail.com>
2019-10-06 19:40:06 -0500

    creando filter y enlazando

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,6 +5,9 @@ import Ready from "../images/1.png";
 import InProgress from "../images/2.png";
 import Complete from "../images/3.png";
 import ButtonAdd from "./ButtonAdd";
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import FilterLogo from "../images/filter.png"
 class Task extends React.Component {
 
 
@@ -12,6 +15,25 @@ class Task extends React.Component {
       return (
         <div>
             <Nav/>
+            <br/>
+            <br/>
+            <br/>
+            <center>
+            <Link to="/taskfilter" style={{textDecoration:"none"}}>
+              <Button
+              style={{ margin: "10px",position: "fixed",
+                            top: "0.2",left:"45%"
+                           ,color:"#fff",zIndex:"100" }}
+                type='submit'
+                
+                variant='contained'
+                color='secondary'
+                className='submit'>
+                Filter
+                <img src={FilterLogo} alt="" srcset=""/>
+              </Button>
+            </Link>
+            </center>
             <CardTask description="Implement View Login" assign="Andres Florez" date="12-05-13" state="ready" imageS={Ready}/>
             <CardTask description="Implement View Register" assign="Andres Florez" date="12-05-03" state="In Progress" imageS={InProgress}/>
             <CardTask description="Implement View Task" assign="Andres Florez" date="11-05-13" state="Completed" imageS={Complete}/>
